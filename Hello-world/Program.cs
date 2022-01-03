34ee7955c6e95e11805066fe303be2a475210ba4
Ricardo Vicentini <ric_vicentini@hotmail.com>
2022-01-03 07:43:47 -0300

    exercises 2.9

---
+++ Hello-world/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+var name = string.Empty;
+
+if (!args.Any())
+    name = "guest";
+else
+    name = string.Join(" ", args);
+
+WriteLine($"Hello, {name}!");
+ReadLine();
